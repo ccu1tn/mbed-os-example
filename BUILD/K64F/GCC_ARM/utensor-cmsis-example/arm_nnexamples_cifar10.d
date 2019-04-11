ebae310febe157526941978d322709bb87505336
ccu1tn <tamdhkhtn@gmail.com>
2019-04-11 16:22:50 +0800

    os build

---
+++ BUILD/K64F/GCC_ARM/utensor-cmsis-example/arm_nnexamples_cifar10.d
@@ -0,0 +1,17 @@
+BUILD/K64F/GCC_ARM/utensor-cmsis-example/arm_nnexamples_cifar10.o: \
+ utensor-cmsis-example/arm_nnexamples_cifar10.cpp \
+ BUILD/K64F/GCC_ARM/mbed_config.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/arm_math.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/core_cm4.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_version.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_compiler.h \
+ mbed-os/cmsis/TARGET_CORTEX_M/cmsis_gcc.h \
+ utensor-cmsis-example/tensorflow-models/cmsis_pretrained/arm_nnexamples_cifar10_parameter.h \
+ utensor-cmsis-example/tensorflow-models/cmsis_pretrained/arm_nnexamples_cifar10_weights.h \
+ arm_nnfunctions.h arm_nnsupportfunctions.h arm_common_tables.h \
+ arm_nn_tables.h \
+ utensor-cmsis-example/tensorflow-models/cmsis_pretrained/arm_nnexamples_cifar10_inputs.h \
+ mbed-os/cmsis/RTE_Components.h \
+ mbed-os/rtos/TARGET_CORTEX/mbed_rtx_conf.h \
+ mbed-os/targets/TARGET_Freescale/mbed_rtx.h \
+ mbed-os/cmsis/mbed_cmsis_conf.h
